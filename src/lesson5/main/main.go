5cd9776f5e250ebcfecd0f878e665418c25e2c8a
s0lus <mashenkoalex@gmail.com>
2016-09-01 13:02:43 +0900

    Rework source code for lesson 5

--- src/lesson5/main/main.go
+++ src/lesson5/main/main.go
@@ -5,67 +5,51 @@ import (
 )
 
 func main() {
-	//array()
-	//slice()
-	mapp()
-}
-
-func array() {
-
 	// Объявление массива (1 вариант)
-	var x [5]float64
+	var x [5]int
+	fmt.Println(x)
 
 	// Объявление массива (2 вариант)
-	y := [5]float64{4.3, 2.2, 3.3, 5.0, 2.1}
+	y := [5]float64{1.2, 2.3, 0.4, 32.3, 32.5}
+	fmt.Println(y)
 
-	fmt.Println("Array x:", x)
-	fmt.Println("Array y:", y)
-
-	// Пример вычисления среднего значения 1
-	var total1 float64 = 0
-	for i := 0; i < len(y); i++ {
-		total1 += y[i]
+	// Нахождение среднего значения
+	marks := [5]float64{2.0, 4.0, 5.0, 3.0, 4.0}
+	sum := 0.0
+	for i := 0; i < len(marks); i++ {
+		sum += marks[i]
 	}
 
-	fmt.Println("Средний балл:", total1 / float64(len(y)))
+	average := sum / float64(len(marks))
+	fmt.Println("Срдний балл:", average)
 
-	// Пример вычисления среднего значения 2
-	var total2 float64 = 0
-	for _, value := range y {
-		total2 += value
-	}
-
-	fmt.Println("Средний балл:", total2 / float64(len(y)))
-}
-
-func slice() {
-	// Создание среза
+	// Создание среза с использованием
 	x := make([]float64, 5, 10)
 	fmt.Println(x)
 
-	// Создание среза, используя [low:high)
+	// Создание среза с использованием slice
 	arr := [5]int{1, 2, 3, 4, 5}
-	fmt.Println(arr[2:4])
+	y := arr[2:4]
+	fmt.Println(y)
 
-	// Функции срезов append() copy()
-	slice1 := []int{1,2,3}
-	slice2 := append(slice1, 4, 5)
-	fmt.Println(slice1, slice2)
+	// append()
+	slice1 := []int{1, 2, 3}
+	slice2 := append(slice1, 4, 5, 6, 7)
+	fmt.Println(slice2)
 
-	// copy
-	slice3 := []int{1,2,3}
-	slice4 := make([]int, 2)
-	copy(slice4, slice3)
-	fmt.Println(slice3, slice4)
-}
+	//copy()
+	slice_1 := []int{1, 2, 3, 4, 5, 6}
+	slice_2 := make([]int, 2)
+	copy(slice_2, slice_1)
+	fmt.Println(slice_1, slice_2)
 
-func mapp() {
-	// Объявление карты
 	var x map[string]int
 	fmt.Println(x)
 
-	// Инициализация карты
-	y := make(map[string]int)
-	y["key"] = 10
-	fmt.Println(y)
+	// Создание и нициализации карты
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println(n)
+
+	delete(n, "foo")
+	fmt.Println(n)
 }
